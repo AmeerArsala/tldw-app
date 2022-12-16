44e221f27e1051b0f3ea307eb645801167945560
Aqeel <aqeelali0312@gmail.com>
2022-12-15 16:02:57 -0800

    landing pg styling + some code

--- frontend/src/components/pages/landing/LandingPage.tsx
+++ frontend/src/components/pages/landing/LandingPage.tsx
@@ -1,6 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./LandingPage.css";
+import UrlTextBox from "./urltextbox"; //maybe the ./ part will cause an error - coudl do two .. ? 
+import TriggerBtn from "./triggerbtn";
 
 export default function LandingPage() {
-    return (<></>);
+  return (
+    <>
+      <header className="landing-header">
+        TLDW
+      </header>
+      <nav>
+        <Link to="/about">About</Link>
+        <Link to="/">TLDW</Link>
+      </nav>
+      <UrlTextBox />
+      <TriggerBtn />
+      <p>or drag and drop a YouTube URL anywhere below!</p>
+    </>
+  );
 }
